102d3724e5c6fe876dbe85173c9ce0b1537b7d52
integreedy <hmmy.ntua@gmail.com>
2020-12-02 02:35:14 +0100

    install cncjs in target

--- board/stmicroelectronics/stm32mp157c-dk2/overlay/workspace/init_script.sh
+++ board/stmicroelectronics/stm32mp157c-dk2/overlay/workspace/init_script.sh
@@ -4,4 +4,6 @@ echo "deingena was here--------------"
 ifconfig eth0 up
 ifconfig wlan0 up
 wpa_supplicant -B -iwlan0 -c /etc/wpa_supplicant.conf
-
+echo nameserver 8.8.8.8 > /etc/resolv.conf
+echo nameserver 8.8.4.4 >> /etc/resolv.conf
+cncjs --allow-remote-access &
