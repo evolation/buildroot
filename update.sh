93363ff4ee4c551210dbdd85a5e455967300dd25
integreedy <hmmy.ntua@gmail.com>
2020-12-04 01:28:26 +0100

    update config 2

--- update.sh
+++ update.sh
@@ -4,4 +4,10 @@ cp /home/integreedy/workspace/buildroot/.config /home/integreedy/workspace/build
 
 cp /home/integreedy/workspace/buildroot/output/build/linux-custom/.config /home/integreedy/workspace/buildroot/evolation_configs/NGjex_deingena_latest.config
 
+git add /home/integreedy/workspace/buildroot/evolation_configs/buildroot_deingena_latest.config
 
+git add /home/integreedy/workspace/buildroot/evolation_configs/NGjex_deingena_latest.config
+
+git commit -m "update configs"
+
+git push
