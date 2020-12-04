0514e0fd4c5d8426de7bed387985894bef125818
integreedy <hmmy.ntua@gmail.com>
2020-12-04 01:14:01 +0100

    add update file

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp /home/integreedy/workspace/buildroot/.config /home/integreedy/workspace/buildroot/evolation_configs/buildroot_deingena_latest.config
+
+git add /home/integreedy/workspace/buildroot/evolation_configs/buildroot_deingena_latest.config
+git commit "update buidlroot"
+git push
+
+
+
+
+
