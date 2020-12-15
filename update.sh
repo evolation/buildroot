8555b69ddca0cd88c04a6cc77bc064d57644661a
integreedy <hmmy.ntua@gmail.com>
2020-12-16 00:56:05 +0100

    ADD spidev and documents

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp /home/integreedy/workspace/buildroot/.config /home/integreedy/workspace/buildroot/evolation_configs/buildroot_deingena_latest.config
+sed '/^#/ d'  < /home/integreedy/workspace/buildroot/.config > /home/integreedy/workspace/buildroot/evolation_configs/buildroot_deingena_latest.config
 
-cp /home/integreedy/workspace/buildroot/output/build/linux-custom/.config /home/integreedy/workspace/buildroot/evolation_configs/NGjex_deingena_latest.config
+sed '/^#/ d'  < /home/integreedy/workspace/buildroot/output/build/linux-custom/.config > /home/integreedy/workspace/buildroot/evolation_configs/NGjex_deingena_latest.config
 
 git add /home/integreedy/workspace/buildroot/evolation_configs/buildroot_deingena_latest.config
 
