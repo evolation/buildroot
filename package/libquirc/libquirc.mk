71faa8f52c20708d7b82a7c51dedd127b4f60df0
integreedy <hmmy.ntua@gmail.com>
2020-12-16 06:42:12 +0100

    packet is downloaded but fails to extract

--- package/libquirc/libquirc.mk
+++ package/libquirc/libquirc.mk
@@ -4,12 +4,25 @@
 #
 ################################################################################
 LIBQUIRC_VERSION = 1.0.0
-LIBQUIRC_SOURCE = libquirc-$(LIBFOO_VERSION).tar.gz
-LIBQUIRC_SITE = https://github.com/evolation/libquirc/blob/master
+LIBQUIRC_SOURCE = libquirc-$(LIBQUIRC_VERSION).tar.gz
+LIBQUIRC_SITE = $(call github,evolation,libquirc,$(LIBQUIRC_VERSION))
 LIBQUIRC_LICENSE = GPL-3.0+
-LIBQUIRC_LICENSE_FILES = COPYING
-LIBQUIRC_INSTALL_STAGING = YES
 
 define LIBQUIRC_BUILD_CMDS
     $(TARGER_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
+
+define LIBQUIRC_EXTRACT_CMDS
+    $(call suitable-extractor,$(LIBQUIRC_SOURCE)) $(LIBQUIRC_DL_DIR)/$(LIBQUIRC_SOURCE) | \
+	$(TAR) --strip-components=1 -C $(@D) $(TAR_OPTIONS) -
+    
+	mv $(@D)/LIBQUIRC-$(LIBQUIRC_VERSION)/* $(@D)
+	$(RM) -r $(@D)/LIBQUIRC-$(LIBQUIRC_VERSION)
+endef
+
+
+define LIBQUIRC_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR="$(TARGET_DIR)" install
+endef
+
+$(eval $(generic-package))
