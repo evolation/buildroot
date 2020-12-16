7770528b65c7be7ec9d95d59118cc97edd16d0d8
integreedy <hmmy.ntua@gmail.com>
2020-12-16 05:30:55 +0100

    add libquirc that still dont build

--- package/libquirc/libquirc.mk
+++ package/libquirc/libquirc.mk
@@ -5,11 +5,11 @@
 ################################################################################
 LIBQUIRC_VERSION = 1.0.0
 LIBQUIRC_SOURCE = libquirc-$(LIBFOO_VERSION).tar.gz
-LIBQUIRC_SITE = https://github.com/evolation/libquirc
+LIBQUIRC_SITE = https://github.com/evolation/libquirc/blob/master
 LIBQUIRC_LICENSE = GPL-3.0+
 LIBQUIRC_LICENSE_FILES = COPYING
 LIBQUIRC_INSTALL_STAGING = YES
 
 define LIBQUIRC_BUILD_CMDS
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+    $(TARGER_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
