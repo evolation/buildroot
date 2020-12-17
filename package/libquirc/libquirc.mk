2ae03bf6a4d3bd0f529c0794be0bf123ac7078c5
integreedy <hmmy.ntua@gmail.com>
2020-12-17 22:43:35 +0100

    add version libquirc 1.0.2

--- package/libquirc/libquirc.mk
+++ package/libquirc/libquirc.mk
@@ -3,19 +3,20 @@
 # libfoo
 #
 ################################################################################
-LIBQUIRC_VERSION = 1.0.0
+LIBQUIRC_VERSION = 1.0.2
 LIBQUIRC_SOURCE = libquirc-$(LIBQUIRC_VERSION).tar.gz
 LIBQUIRC_SITE = $(call github,evolation,libquirc,$(LIBQUIRC_VERSION))
 LIBQUIRC_LICENSE = GPL-3.0+
-LIBQUIRC_DEPENDENCIES = sdl
+LIBQUIRC_DEPENDENCIES = sdl libpng jpeg sdl_gfx
 LIBQUIRC_INSTALL_STAGING = YES
+
 define LIBQUIRC_BUILD_CMDS
-    $(TARGER_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) quirc-scanner
+    $(TARGER_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS="-O3 -Wall -fPIC" SDL_CFLAGS="-D_GNU_SOURCE=1 -D_REENTRANT -I$(TARGET_DIR)/usr/include/SDL" SDL_LIBS="-lSDL" -C $(@D) all
 endef
 
 
-#define LIBQUIRC_INSTALL_TARGET_CMDS
-#	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR="$(TARGET_DIR)" install
-#endef
+define LIBQUIRC_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR="$(TARGET_DIR)" install
+endef
 
 $(eval $(generic-package))
